cbe522edec9d3a8b3d4d56edb436840f9cb090be
José Mira <jose.rui.mira@gmail.com>
2020-04-27 20:23:40 +0100

    add LiteQueryable IncludeAll extension

---
+++ abremir.postcrossing.engine/Extensions/LiteQueryableExtension.cs
@@ -0,0 +1,55 @@
+﻿using LiteDB;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace abremir.postcrossing.engine.Extensions
+{
+    public static class LiteQueryableExtension
+    {
+        public static ILiteQueryable<T> IncludeAll<T>(this ILiteQueryable<T> liteQueryable) where T : class
+        {
+            return liteQueryable.Include(GetRecursivePaths(typeof(T)));
+        }
+
+        private static List<BsonExpression> GetRecursivePaths(Type pathType, string basePath = null)
+        {
+            var fields = pathType.GetProperties()
+                .Where(property =>
+                    !property.PropertyType.IsValueType
+                    && (property.GetCustomAttribute<BsonRefAttribute>() != null
+                        || typeof(IEnumerable).IsAssignableFrom(property.PropertyType) && property.PropertyType != typeof(string)));
+
+            var paths = new List<BsonExpression>();
+
+            basePath = string.IsNullOrEmpty(basePath) ? "$" : basePath;
+
+            foreach (var field in fields)
+            {
+                var path = typeof(IEnumerable).IsAssignableFrom(field.PropertyType)
+                    ? $"{basePath}.{field.Name}[*]"
+                    : $"{basePath}.{field.Name}";
+
+                if (field.GetCustomAttribute<BsonRefAttribute>() != null)
+                {
+                    paths.Add(path);
+                }
+
+                paths.AddRange(GetRecursivePaths(field.PropertyType.GetInnerGenericType(), path));
+            }
+
+            return paths;
+        }
+
+        public static Type GetInnerGenericType(this Type type)
+        {
+            // Attempt to get the inner generic type
+            Type innerType = type.GetGenericArguments().FirstOrDefault();
+
+            // Recursively call this function until no inner type is found
+            return innerType is null ? type : innerType.GetInnerGenericType();
+        }
+    }
+}
